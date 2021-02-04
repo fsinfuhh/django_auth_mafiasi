c15cce0aa0537e82c6828faa33aa3c291d34e521
Finn-Thorben Sell <dev@finn-thorben.me>
2021-02-04 21:43:54 +0100

    make username field NULL instead of blank
    this was done because blank values run into the unique constraints while
    NULL values do not

--- src/django_auth_mafiasi/models.py
+++ src/django_auth_mafiasi/models.py
@@ -6,7 +6,9 @@ from django.db import models
 
 class MafiasiAuthModelUser(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    username = models.CharField(max_length=150, unique=True, blank=True)
+
+    # username is null but not blank because blank fields run into unique restrictions
+    username = models.CharField(max_length=150, unique=True, null=True)
 
     REQUIRED_FIELDS = []
 
